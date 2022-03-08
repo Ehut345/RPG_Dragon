61be1cc23d2be33b7476528aef8592ccbfbe1f8d
Ehut345 <91867605+Ehut345@users.noreply.github.com>
2022-03-08 10:52:59 +0530

    dialog of player done

--- Assets/Scripts/DialogObjects.cs
+++ Assets/Scripts/DialogObjects.cs
@@ -56,14 +56,11 @@ public class DialogObjects : MonoBehaviour
             PlayDialog(currentDialog);
         }
     }
-    // Start is called before the first frame update
     void Start()
     {
         //player = FindObjectOfType<Player>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
 
--- Assets/Scripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -6,33 +6,41 @@ public class NonPlayerCharacter : MonoBehaviour
 {
     public GameObject infoText;
     public GameObject diaglogText;
+    bool tempInfotexactive = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            infoText.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.K))
+            //if (tempInfotexactive == false)
             {
-                other.gameObject.GetComponent<Player>().dialogNumber = 1;
-                diaglogText.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.K))
+                {
+                    other.gameObject.GetComponent<Player>().dialogNumber = 1;
+                    diaglogText.SetActive(true);
+                    infoText.SetActive(false);
+                    //tempInfotexactive = true;
+                }
             }
         }
-
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        infoText.SetActive(true);
     }
     private void OnTriggerExit(Collider other)
     {
         infoText.SetActive(false);
-        diaglogText.SetActive(false);
+        //diaglogText.SetActive(false);
     }
 }
